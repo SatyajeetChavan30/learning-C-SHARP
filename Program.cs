cc47e66cab1b582f82574e9feedd9074be889563
SatyajeetChavan30 <satyajeetchavan30@gmail.com>
2025-07-13 19:17:35 +0530

    add file

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass satyajeet = new myClass(17,"satyajeet" ,87);
+            Console.WriteLine(satyajeet.ToString());
+            Console.WriteLine(satyajeet.getAge());
+            Console.WriteLine(satyajeet.getName());
+            Console.WriteLine(satyajeet.getPercent());
+            Console.WriteLine(satyajeet.GetType());
+            //Console.WriteLine(satyajeet.);
+
+        }
+
+        class myClass
+        {
+            int age;
+            string name;
+            float percent;
+            public myClass(int age, string name, float percent)
+            {
+                this.age = age;
+                this.name = name;
+                this.percent = percent;
+            }
+            public int getAge() { return age; }
+            public string getName() { return name; }
+            public float getPercent() { return percent; }
+
+        }
+    }
+}
